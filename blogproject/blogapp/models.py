766dcb4c77d7fd9b45f4c4cb6fa28b963e9c788e
syKim12 <fromsykim@gmail.com>
2022-05-22 20:31:58 +0900

    upload comment model

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,4 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
@@ -9,3 +10,11 @@ class Blog(models.Model):
 
     def __str__(self):
         return self.title
+
+class Comment(models.Model):
+    comment = models.CharField(max_length=200)
+    date = models.DateTimeField(auto_now_add=True)
+    post = models.ForeignKey(Blog, on_delete = models.CASCADE)
+
+    def __str__(self):
+        return self.comment
